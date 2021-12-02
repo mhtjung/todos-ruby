7e246ff813ea42306896211e7b0a8b0d8bc2cb33
Mike Jung <themikejung@gmail.com>
2021-12-02 12:19:11 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+ruby "3.0.2"
+gem "sinatra", "~>1.4.7"
+gem "sinatra-contrib"
+gem "erubis"
+gem "puma"
+gem "pg"
---
+++ database_persistence.rb
@@ -0,0 +1,76 @@
+require "pg"
+class DatabasePersistence
+  def initialize(logger)
+    @db = PG.connect(dbname: "todos")
+    @logger = logger
+  end
+
+  def query(sql, *params)
+    @logger.info "#{sql} : #{params}"
+    @db.exec_params(sql, params)
+  end
+
+  def find_list(id)
+    sql = "SELECT * FROM lists WHERE id = $1"
+    result = query(sql, id)
+    tup = result.first
+    todos = find_todos_for_list(id)
+    {id: tup["id"], name: tup["name"], todos: todos}
+  end
+
+  def find_todos_for_list(list_id)
+    sql = "SELECT * FROM todos WHERE list_id = $1"
+    result = query(sql, list_id)
+    result.map do |todo_tuple|
+      { id: todo_tuple["id"].to_i,
+        name: todo_tuple["name"],
+        completed: todo_tuple["completed"] == "t",
+        list_id: list_id
+      } 
+    end
+  end
+
+  def all_lists
+    sql = "SELECT * from LISTS"
+    result = query(sql)
+    result.map do |tup|
+      todos = find_todos_for_list(tup["id"])
+      {id: tup["id"], name: tup["name"], todos: todos}
+    end
+  end
+
+  def create_new_list(list_name)
+    sql = "INSERT INTO lists (name) VALUES ($1)"
+    query(sql, list_name)
+  end
+
+  def delete_list(id)
+    sql = "DELETE FROM lists WHERE id = $1"
+    query(sql, id)
+  end
+
+  def update_list_name(id, new_name)
+    sql = "UPDATE lists SET name = $1 WHERE id = $2"
+    query(sql, new_name, id)
+  end
+
+  def create_new_todo(list_id, todo_name)
+    sql = "INSERT INTO todos (name, list_id) VALUES ($1, $2)"
+    query(sql, todo_name, list_id)
+  end
+
+  def delete_todo_from_list(list_id, todo_id)
+    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
+    query(sql, todo_id, list_id)
+  end
+
+  def update_todo_status(list_id, todo_id, new_status)
+    sql = "UPDATE TODOS SET completed = $1 WHERE id = $2 and list_id = $3"
+    query(sql, new_status, todo_id, list_id)
+  end
+
+  def mark_all_todos_as_completed(list_id)
+    sql = "UPDATE TODOS set completed = 't' WHERE list_id = $1"
+    query(sql, list_id)
+  end
+end
